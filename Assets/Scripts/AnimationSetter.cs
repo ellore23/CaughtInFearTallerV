1f7f93420bb2fa214872a4af90953e708bc70239
ellore23 <florezvalencialorenzo@gmail.com>
2022-10-31 18:13:18 -0500

    NictoAvances

--- Assets/Scripts/AnimationSetter.cs
+++ Assets/Scripts/AnimationSetter.cs
@@ -6,11 +6,29 @@ public class AnimationSetter : MonoBehaviour
 {
     public GameObject fondo;
     public GameObject animacionInicial;
-
+    public Animator nicto;
+    
+   
     private void Awake()
     {
+        nicto = GameObject.Find("Animation").GetComponent<Animator>();
         fondo.SetActive(false);
         animacionInicial.SetActive(true);
+    }
+
+    private void Update()
+    {
+        EndInitialAnim();
+       
+    }
+    private void EndInitialAnim()
+    {
+        if (nicto.GetCurrentAnimatorStateInfo(0).normalizedTime > 1 && !nicto.IsInTransition(0))
+        {
+            fondo.SetActive(true);
+            animacionInicial.SetActive(false);
+        }
+       
     }
 
 }
